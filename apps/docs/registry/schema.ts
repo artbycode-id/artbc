8da645583a30c31b4db65394f84b439cb34eeae9
anggasptr <saputraa170503@gmail.com>
2024-05-11 13:26:56 +0700

    feat: alert component

---
+++ apps/docs/registry/schema.ts
@@ -0,0 +1,37 @@
+import { z } from "zod"
+
+export const blockChunkSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+  component: z.any(),
+  file: z.string(),
+  code: z.string().optional(),
+  container: z
+    .object({
+      className: z.string().nullish(),
+    })
+    .optional(),
+})
+
+export const registryEntrySchema = z.object({
+  name: z.string(),
+  description: z.string().optional(),
+  dependencies: z.array(z.string()).optional(),
+  devDependencies: z.array(z.string()).optional(),
+  registryDependencies: z.array(z.string()).optional(),
+  files: z.array(z.string()),
+  source: z.string().optional(),
+  type: z.enum([
+    "components:ui",
+    "components:component",
+    "components:example",
+    "components:block",
+  ]),
+  category: z.string().optional(),
+  subcategory: z.string().optional(),
+  chunks: z.array(blockChunkSchema).optional(),
+})
+
+export const registrySchema = z.array(registryEntrySchema)
+
+export type Registry = z.infer<typeof registrySchema>
